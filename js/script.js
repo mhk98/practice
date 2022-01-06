510fa619c88ac250346ac2511a94d859df1e0e49
Md Mohsin Kabir <mohsinkabirseo@gmail.com>
2022-01-06 15:49:25 +0600

    practice

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+    
+$('#banner').slick({
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: false,
+    fade: true,
+    // vertical: true,
+
+    
+  });
+})
